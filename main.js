1e2955eba0512c3fc66411b04c45ec0cf0605b39
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-05-27 23:19:50 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+(async () => {
+    const { duration } = await inquirer.prompt([
+        {
+            type: 'number',
+            name: 'duration',
+            message: 'Enter the duration of the timer in seconds:',
+            validate: (input) => input > 0 || 'Please enter a valid positive number of seconds.'
+        }
+    ]);
+    console.log(chalk.green("Timer started!"));
+    for (let i = duration; i > 0; i--) {
+        console.log(chalk.blue(`Remaining time: ${i} seconds`));
+        let make = await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+    console.log(chalk.red("Timer ended!"));
+})();
